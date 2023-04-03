ee457869c64e8996a6e2f325e175a274ebb6437c
imac <frank.siyli@gmail.com>
2023-04-03 16:21:13 +0200

    delete axios and just run on swr

--- src/components/FooterButton/FooterButton.js
+++ src/components/FooterButton/FooterButton.js
@@ -3,7 +3,8 @@ import Link from "next/link"
 
 export default function FooterButton ({title}) {
   return (<>
-    <h2>
+            <hr/>
+            <h2>
             <Link href={"/"}>{title}</Link>
             </h2>
     </>
