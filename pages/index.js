34a685c3f47c06f590cd16ef44a8054c48ae9429
imac <frank.siyli@gmail.com>
2023-04-03 16:15:30 +0200

    add dynamic Button component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-
 import Header from "../src/components/Header/Header";
 import WeekCard from "../src/components/WeekCard/WeekCard";
+import FooterButton from "../src/components/FooterButton/FooterButton";
 
 export default function HomePage() {
 
@@ -8,6 +8,7 @@ export default function HomePage() {
     <div>
       <Header/>
       <WeekCard/>
+      <FooterButton href="page2" title="Create Plan"></FooterButton>
       
     </div>
   );
