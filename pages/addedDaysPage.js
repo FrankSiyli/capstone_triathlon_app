1d9ea29c6fe3ba238d895b04611ef5fe4a3cb9b7
imac <frank.siyli@gmail.com>
2023-04-05 11:43:48 +0200

    fix console error

--- pages/addedDaysPage.js
+++ pages/addedDaysPage.js
@@ -1,28 +1,20 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import useStore from "../src/store";
 
 function AddedDaysPage() {
   const { days } = useStore();
 
   useEffect(() => {
-    // Get the stored state from local storage
-    const storedState = JSON.parse(localStorage.getItem("days-storage"));
-
-    // If the stored state has added days, update the days in the store
-    if (storedState && Array.isArray(storedState.days)) {
-      const addedDays = storedState.days.filter((day) => day.added);
-      useStore.setState({ days: addedDays });
-    }
+    const addedDays = days.filter((day) => day.added);
+    useStore.setState({ days: addedDays });
   }, []);
 
   return (
     <div>
       <h1>Added Days</h1>
-      <ul>
-        {days.map((day) => (
-          <li key={day.id}>{day.title}</li>
-        ))}
-      </ul>
+      {days.map((day) => (
+        <div key={day.id}>{day.title}</div>
+      ))}
     </div>
   );
 }
