2a3141a1e632e32d4b13fe7b89ad86a366b37109
imac <frank.siyli@gmail.com>
2023-04-05 11:07:17 +0200

    add toggle button to each day

--- src/store.js
+++ src/store.js
@@ -2,27 +2,25 @@ import { create } from "zustand";
 
 const useStore = create((set) => ({
   days: [
-    {
-      title: "Monday",
-    },
-    {
-      title: "Tuesday",
-    },
-    {
-      title: "Wednesday",
-    },
-    {
-      title: "Thursday",
-    },
-    {
-      title: "Friday",
-    },
-    {
-      title: "Saturday",
-    },
-    {
-      title: "Sunday",
-    },
+    { id: 1, title: "Monday", added: false },
+    { id: 2, title: "Tuesday", added: false },
+    { id: 3, title: "Wednesday", added: false },
+    { id: 4, title: "Thursday", added: false },
+    { id: 5, title: "Friday", added: false },
+    { id: 6, title: "Saturday", added: false },
+    { id: 7, title: "Sunday", added: false },
   ],
+  toggleDay: (dayId) => {
+    set((state) => {
+      const dayIndex = state.days.findIndex((day) => day.id === dayId);
+      const addedDays = [...state.days];
+      addedDays[dayIndex] = {
+        ...addedDays[dayIndex],
+        added: !addedDays[dayIndex].added,
+      };
+      return { days: addedDays };
+    });
+  },
 }));
+
 export default useStore;
