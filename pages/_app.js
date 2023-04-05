93b4c263761b4011864773bc47a9b52fcb6c9d6e
imac <frank.siyli@gmail.com>
2023-04-05 14:59:39 +0200

    delete comment

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,11 @@
 import GlobalStyle from "../styles";
-import useSWR from "swr";
 import Header from "../src/components/Header/Header";
 
-/* const fetcher = (url) => fetch(url).then((res) => res.json());
-
 export default function App({ Component, pageProps }) {
-  const { data, error } = useSWR("/api/db", fetcher);
-
-  if (error) return <div>Failed to load data</div>;
-  if (!data) return <div>Loading training days</div>; */
-  export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Header/>
-       
+      <Header />
       <Component {...pageProps} />
     </>
   );
