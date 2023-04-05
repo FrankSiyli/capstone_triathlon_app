d0a29b5041b04503359eaf24986b793de73f79e9
imac <frank.siyli@gmail.com>
2023-04-05 11:07:17 +0200

    add toggle button to each day

--- src/components/Days/Days.js
+++ src/components/Days/Days.js
@@ -3,11 +3,20 @@ import useStore from "../../store";
 
 export default function Days() {
   const days = useStore((state) => state.days);
+  const toggleDay = useStore((state) => state.toggleDay);
+
   return (
     <>
       <p>we have {days.length} days</p>
       {days.map((day) => {
-        return <p key={day.id}>{day.title}</p>;
+        return (
+          <div key={day.id}>
+            <p>{day.title}</p>
+            <button onClick={() => toggleDay(day.id)}>
+              {day.added ? "✅ " : "❌"}
+            </button>
+          </div>
+        );
       })}
     </>
   );
