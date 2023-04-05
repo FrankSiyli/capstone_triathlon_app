efa25d8c1ac8a33545d348d039cf71d40eb867ac
imac <frank.siyli@gmail.com>
2023-04-05 13:19:33 +0200

    add hamburger menu to each added day

--- pages/addedDaysPage.js
+++ pages/addedDaysPage.js
@@ -4,12 +4,37 @@ import useStore from "../src/store";
 function AddedDaysPage() {
   const { days } = useStore();
   const addedDays = days.filter((day) => day.added);
+
+  const handleDropdownChange = (event, dayId) => {
+    const selectedOption = event.target.value;
+  };
+
   return (
     <>
       <div>
         <h2>Your Training Days</h2>
         {addedDays.map((day) => (
-          <p key={day.id}>{day.title}</p>
+          <div key={day.id}>
+            <h3>{day.title}</h3>
+            <select onChange={(event) => handleDropdownChange(event, day.id)}>
+              <option value="option1">
+                <span
+                  role="button"
+                  onClick={(event) => handleDropdownChange(event, day.id)}
+                >
+                  🍔
+                </span>
+              </option>
+              <option value="option2">
+                <span
+                  role="button"
+                  onClick={(event) => handleDropdownChange(event, day.id)}
+                >
+                  🍔
+                </span>
+              </option>
+            </select>
+          </div>
         ))}
         <FooterButton href="/" title="Back" rel="noopener noreferrer" />
       </div>
