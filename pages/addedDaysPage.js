c1ff0429cf1f8681421bc8f1b067bcf396fd7502
imac <frank.siyli@gmail.com>
2023-04-07 00:53:35 +0200

    a random session get displayed on button click

--- pages/addedDaysPage.js
+++ pages/addedDaysPage.js
@@ -1,39 +1,28 @@
+import React, { useState } from "react";
 import FooterButton from "../src/components/Button/FooterButton";
-import useStore from "../src/store";
+import useStore from "./dayStore";
 
 function AddedDaysPage() {
   const { days } = useStore();
   const addedDays = days.filter((day) => day.added);
-
-  const handleDropdownChange = (event, dayId) => {
-    const selectedOption = event.target.value;
+  const [randomTitle, setRandomTitle] = useState("");
+  const handleRandomTitleClick = () => {
+    const sessions = useStore.getState().sessions;
+    const randomSession = sessions[Math.floor(Math.random() * sessions.length)];
+    setRandomTitle(randomSession.title);
   };
 
   return (
     <>
+      <button id="lalala" onClick={handleRandomTitleClick}>
+        lalala
+      </button>
+      {randomTitle && <p>{randomTitle}</p>}
       <div>
         <h2>Your Training Days</h2>
         {addedDays.map((day) => (
           <div key={day.id}>
             <h3>{day.title}</h3>
-            <select onChange={(event) => handleDropdownChange(event, day.id)}>
-              <option value="option1">
-                <span
-                  role="button"
-                  onClick={(event) => handleDropdownChange(event, day.id)}
-                >
-                  🍔
-                </span>
-              </option>
-              <option value="option2">
-                <span
-                  role="button"
-                  onClick={(event) => handleDropdownChange(event, day.id)}
-                >
-                  🍔
-                </span>
-              </option>
-            </select>
           </div>
         ))}
         <FooterButton href="/" title="Back" />
--- src/components/Days/Days.js
+++ src/components/Days/Days.js
@@ -1,5 +1,5 @@
 import React from "react";
-import useStore from "../../store";
+import useStore from "../../../pages/dayStore";
 
 export default function Days({ filtered }) {
   const days = useStore((state) => {
