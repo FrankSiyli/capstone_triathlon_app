f00d0eb3b99e15a3f9cebbbaa5dae26f08ce0a3e
imac <frank.siyli@gmail.com>
2023-04-11 19:27:19 +0200

    US3 ready

--- pages/addedDaysPage.js
+++ pages/addedDaysPage.js
@@ -1,12 +1,39 @@
+import { useState } from "react";
 import FooterButton from "../src/components/Button/FooterButton";
 import useStore from "../src/store";
 
-function AddedDaysPage() {
-  const { days } = useStore();
-  const addedDays = days.filter((day) => day.added);
+const getTwoRandomSessions = (sessions) => {
+  const randomSessions = sessions.map(
+    () => sessions[Math.floor(Math.random() * sessions.length)]
+  );
+  const firstSession = { ...randomSessions[0], visible: false };
+  const remainingSessions = randomSessions.filter(
+    (session) => session.type !== firstSession.type
+  );
+  const secondSession = {
+    ...remainingSessions[Math.floor(Math.random() * remainingSessions.length)],
+    visible: false,
+  };
+  return [firstSession, secondSession];
+};
 
-  const handleDropdownChange = (event, dayId) => {
-    const selectedOption = event.target.value;
+function AddedDaysPage() {
+  const { days, sessions } = useStore();
+  const addedDays = days.filter((day) => day.added);
+  const [daySessions, setDaySessions] = useState({});
+  const [showSessions, setShowSessions] = useState({});
+  const toggleSessions = (dayId) => {
+    if (!daySessions[dayId]) {
+      const [firstSession, secondSession] = getTwoRandomSessions(sessions);
+      setDaySessions((prevState) => ({
+        ...prevState,
+        [dayId]: [firstSession, secondSession],
+      }));
+    }
+    setShowSessions((prevState) => ({
+      ...prevState,
+      [dayId]: !prevState[dayId],
+    }));
   };
 
   return (
@@ -15,25 +42,23 @@ function AddedDaysPage() {
         <h2>Your Training Days</h2>
         {addedDays.map((day) => (
           <div key={day.id}>
-            <h3>{day.title}</h3>
-            <select onChange={(event) => handleDropdownChange(event, day.id)}>
-              <option value="option1">
-                <span
-                  role="button"
-                  onClick={(event) => handleDropdownChange(event, day.id)}
-                >
-                  🍔
-                </span>
-              </option>
-              <option value="option2">
-                <span
-                  role="button"
-                  onClick={(event) => handleDropdownChange(event, day.id)}
-                >
-                  🍔
-                </span>
-              </option>
-            </select>
+            <h3>
+              {day.title}
+              <button onClick={() => toggleSessions(day.id)}>
+                {showSessions[day.id] ? "x" : "☰"}
+              </button>
+            </h3>
+            {showSessions[day.id] && (
+              <ul>
+                {daySessions[day.id]?.map((session) => (
+                  <li key={session.title}>
+                    <span>{session.icon}</span>
+                    {session.title}
+                    <button>☰</button>
+                  </li>
+                ))}
+              </ul>
+            )}
           </div>
         ))}
         <FooterButton href="/" title="Back" />
