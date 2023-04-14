dcb8949f58f78700be6be1c6dea7b0a8f26850b5
Frank Siyli <frank.siyli@gmail.com>
2023-04-14 15:06:19 +0200

    add toggle event distance buttons

--- pages/addedDaysPage.js
+++ pages/addedDaysPage.js
@@ -14,15 +14,23 @@ const getTwoRandomSessions = (sessions) => {
     ...remainingSessions[Math.floor(Math.random() * remainingSessions.length)],
     visible: false,
   };
+  const unequalSessions = randomSessions.filter(
+    (session) => session.title !== firstSession.title,
+    (session) => session.title !== secondSession.title
+  );
   return [firstSession, secondSession];
 };
 
 function AddedDaysPage() {
   const { days, sessions } = useStore();
+
   const addedDays = days.filter((day) => day.added);
+
   const [daySessions, setDaySessions] = useState({});
   const [showSessions, setShowSessions] = useState({});
   const [showDetails, setShowDetails] = useState({});
+  const [activeButton, setActiveButton] = useState("");
+
   const toggleSessions = (dayId) => {
     if (!daySessions[dayId]) {
       const [firstSession, secondSession] = getTwoRandomSessions(sessions);
@@ -44,8 +52,26 @@ function AddedDaysPage() {
     }));
   };
 
+  const handleButtonClick = (event) => {
+    const value = event.target.value;
+    setActiveButton(value);
+  };
+
   return (
     <>
+      <h2>Choose your event distance</h2>
+      <h3>Short Distance Tri</h3>
+      <button value="short" onClick={handleButtonClick}>
+        {activeButton === "short" ? "active" : "not active"}
+      </button>
+      <h3>Middle Distance Tri</h3>
+      <button value="mid" onClick={handleButtonClick}>
+        {activeButton === "mid" ? "active" : "not active"}
+      </button>
+      <h3> Long Distance Tri</h3>
+      <button value="long" onClick={handleButtonClick}>
+        {activeButton === "long" ? "active" : "not active"}
+      </button>
       <div>
         <h2>Your Training Days</h2>
         {addedDays.map((day) => (
