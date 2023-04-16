ad1d2fedcfca3d9f889b87d7e7623bd021918723
imac <frank.siyli@gmail.com>
2023-04-16 23:23:23 +0200

    days are toggleable and session icons are shown

--- pages/addedDaysPage.js
+++ pages/addedDaysPage.js
@@ -1,9 +1,25 @@
 import FooterButton from "../src/components/FooterButton/";
 import useStore from "../src/store";
+import { useState } from "react";
 
 function AddedDaysPage() {
   const { days } = useStore();
   const addedDays = days.filter((day) => day.added);
+  const [showSessions, setShowSessions] = useState({});
+
+  const toggleSessions = (dayId) => {
+    setShowSessions((previousState) => ({
+      ...previousState,
+      [dayId]: !previousState[dayId],
+    }));
+  };
+
+  const toggleDay = (dayId) => {
+    setShowSessions((previousState) => ({
+      ...previousState,
+      [dayId]: false,
+    }));
+  };
 
   return (
     <>
@@ -11,10 +27,22 @@ function AddedDaysPage() {
         <h2>Your Training Days</h2>
         {addedDays.map((day) => (
           <div key={day.id}>
-            <h3>{day.title}</h3>
-            {day.sessions.map((session) => (
-              <p key={session.id}>{session.title}</p>
-            ))}
+            <h3>
+              {day.title}
+              <button onClick={() => toggleSessions(day.id)}>
+                {showSessions[day.id] ? "Hide Sessions" : "Show Sessions"}
+              </button>
+            </h3>
+            {showSessions[day.id] && (
+              <ul>
+                {day.sessions.map((session) => (
+                  <li key={session.id}>
+                    {session.icon}
+                    {session.title}
+                  </li>
+                ))}
+              </ul>
+            )}
           </div>
         ))}
         <FooterButton href="/" title="Back" />
