61f41666cf65f76b9bd3cbfb91d0c8513fe3f48b
imac <frank.siyli@gmail.com>
2023-04-19 14:57:09 +0200

    next try

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function HomePage() {
   const { days, toggleDay, sessions } = useStore();
   const addedDays = days && days.filter((day) => day.added);
   const [selectedType, setSelectedType] = useState("short");
+  const [isLoading, setIsLoading] = useState(true);
 
   const generateSessionsForDays = useCallback(
     (days) => {
@@ -34,8 +35,16 @@ export default function HomePage() {
   );
 
   useEffect(() => {
-    generateSessionsForDays(addedDays);
-  }, [addedDays, generateSessionsForDays]);
+    if (!isLoading && addedDays) {
+      generateSessionsForDays(addedDays);
+    }
+  }, [isLoading, addedDays, generateSessionsForDays]);
+
+  useEffect(() => {
+    if (days) {
+      setIsLoading(false);
+    }
+  }, [days]);
 
   function handleCreatePlanClick() {
     generateSessionsForDays(addedDays);
