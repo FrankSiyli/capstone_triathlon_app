2ba31f7450eec94635a71c2a6a39cd34cf22a1b1
imac <frank.siyli@gmail.com>
2023-04-21 20:37:53 +0200

    vercel my old slow friend

--- pages/index.js
+++ pages/index.js
@@ -21,27 +21,27 @@ export default function HomePage() {
   });
 
   function generateSessionsForDays() {
-    if (error) return <div>Error loading sessions</div>;
-    if (!sessions) return <div>Loading sessions...</div>;
-    addedDays.forEach((day) => {
-      if (!day.added) {
-        day.sessions = [];
-      } else {
-        const remainingSessions = sessions.filter(
-          (session) => session.eventDistance === selectedType && session.type
-        );
-        const selectedSessions = [];
-        for (let i = 0; i < 2; i++) {
-          const randomIndex = Math.floor(
-            Math.random() * remainingSessions.length
+    if (sessions && addedDays) {
+      addedDays.forEach((day) => {
+        if (!day.added) {
+          day.sessions = [];
+        } else {
+          const remainingSessions = sessions.filter(
+            (session) => session.eventDistance === selectedType && session.type
           );
-          const session = remainingSessions[randomIndex];
-          selectedSessions.push(session);
-          remainingSessions.splice(randomIndex, 1);
+          const selectedSessions = [];
+          for (let i = 0; i < 2; i++) {
+            const randomIndex = Math.floor(
+              Math.random() * remainingSessions.length
+            );
+            const session = remainingSessions[randomIndex];
+            selectedSessions.push(session);
+            remainingSessions.splice(randomIndex, 1);
+          }
+          day.sessions = selectedSessions;
         }
-        day.sessions = selectedSessions;
-      }
-    });
+      });
+    }
   }
 
   function handleCreatePlanClick() {
@@ -59,14 +59,22 @@ export default function HomePage() {
         selectedType={selectedType}
         setSelectedType={setSelectedType}
       />
-      <Days onToggle={handleDayToggle} />
-      <Link
-        href="/addedDaysPage"
-        title="Create Plan"
-        onClick={handleCreatePlanClick}
-      >
-        <h2>Create Plan</h2>
-      </Link>
+      {error ? (
+        <p>Error loading sessions data</p>
+      ) : !sessions ? (
+        <p>Loading sessions data...</p>
+      ) : (
+        <>
+          <Days onToggle={handleDayToggle} />
+          <Link
+            href="/addedDaysPage"
+            title="Create Plan"
+            onClick={handleCreatePlanClick}
+          >
+            <h2>Create Plan</h2>
+          </Link>
+        </>
+      )}
     </div>
   );
 }
